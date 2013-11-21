78bf5a17c0a4663107d9ea802a0488406255a143
001235743 <001235743@N206C4.labs.neit.edu>
2013-11-20 20:35:56 -0500

    Week 4 update

--- Week 4/index.php
+++ Week 4/index.php
@@ -1,8 +1,46 @@
+<!DOCTYPE html>
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+            <title></title>
+         </head>
+    <body>
+        
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+            session_start();
+        
+        include 'Validator.php';
+        include 'Config.php';
+        
+        
+        
+        $testEmail = "test@email.com";
+        
+    if(Validator::emailIsValid($testEmail))
+        {
+            echo "EMAIL IS VALID";            
+        }
+    else
+        {
+    echo "EMAIL IS <strong>NOT</strong> VALID";
+        }
+        
+       
+        $dbh = new PDO(Config::DB_DNS, Config::DB_USER, Config::DB_PASSWORD);
+        
+        $stmt = $dbh->prepare('SELECT * FROM week3');
+                                                                                                                                                                             
+        $stmt->execute();
+        
+        $result = $stmt->fetchAll();
+        
+        print_r($result);
+        
+        
+        echo "<h1>",$_SESSION["counter"],"</h1>";
+?>
+        
+    </body>
+</html>
