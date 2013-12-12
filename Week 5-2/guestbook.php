6b103639681663b52cddd8697a49b0cb9e931334
001235743 <001235743@N206C4.labs.neit.edu>
2013-12-11 21:59:16 -0500

    Week5-2 update

---
+++ Week 5-2/guestbook.php
@@ -0,0 +1,53 @@
+<?php include 'dependency.php'; ?>
+<!--
+To change this template, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>My Guestbook</title>
+    </head>
+    <body>
+        
+        
+ <?php
+ 
+        // put your code here
+        Login::confirmAccess();
+        
+        $gb = new Guestbook();
+        
+        if(! $gb->processGuestbook()){
+            echo "there's an issue\t ";
+        }
+        $gb->displayGuestbook();        
+        ?>
+        <div id="form">
+        <form action="#" method="post">
+             Name: <input type="text" name="name" value=""/> 
+            <br />           
+            Email: <input type="text" name="email" value=""/> 
+            <br />    
+            Comments: <input type="text" name="comments" value=""/> 
+            <br /> 
+            <input  type="submit"  value="Submit"/>
+        </form>
+        </div>
+        <style>
+            body{
+                text-align:center;
+                color: whitesmoke;
+                background-color:black;
+            }
+            
+            input{
+                color: whitesmoke;
+                background-color: black;
+            }
+            
+            
+        </style>
+    </body>
+</html>
